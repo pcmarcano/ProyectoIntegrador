43efe7dc6d8aca993894df1a98574e19e1533e03
JPRam89 <juan.ramireza@usach.cl>
2024-05-15 15:32:57 -0400

    cambio routes js a jsx

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from "../components/pages/home/Home.jsx";
 import Formulario from "../components/pages/home/formulario/Formulario.jsx";
 import Search from "../components/pages/home/Search.jsx";
+import Listado from "../components/pages/home/listado/Listado.jsx";
 
 const HomeAndSearch = () => (
     <div>
@@ -20,5 +21,10 @@ export const routes = [
         id: "formulario",
         path: "/form",
         Element: Formulario,
+    },
+    {
+        id: "listado",
+        path: "/list",
+        Element: Listado,
     },
 ];
