a833353470e772582f304c30dfe24a125a965116
JPRam89 <juan.ramireza@usach.cl>
2024-05-16 10:53:28 -0400

    barra search footer y responsive

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from "../../../../public/logo2.png";
+import logo from "../../../../public/logo1.png";
 import './Footer.css';
 
 const Footer = () => (
     <div className="footer-container">
         <img src={logo} alt="Logo" className="logo" />
-        <p className="copyright">&copy; Spacio 2024</p>
+        <p className="copyright">Copyright &copy; 2024 | Spacio</p>
     </div>
 );
 
