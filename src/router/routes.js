14dce1f5d36e09349454bd778a669c68fdb46206
Intime-SA <invertimesa@gmail.com>
2024-05-16 14:19:49 -0300

    cambios routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,12 +2,20 @@ import Home from "../components/pages/home/Home";
 import Formulario from "../components/pages/home/formulario/Formulario.jsx";
 import EspacioVista from "../components/pages/home/formulario/espacio/EspacioVista.jsx";
 import Listado from "../components/pages/home/listado/Listado.jsx";
+import Box from "@mui/material/Box";
+
+const HomeAndSearch = () => (
+  <Box sx={{ margin: 0, padding: 0 }}>
+    <Search />
+    <Home />
+  </Box>
+);
 
 export const routes = [
   {
     id: "home",
     path: "/",
-    Element: Home,
+    Element: HomeAndSearch,
   },
   {
     id: "formulario",
