d2ce090c6895f8604717671edb3a9241cdd6a31e
Intime-SA <invertimesa@gmail.com>
2024-05-16 16:33:04 -0300

    cambio footer

---
+++ src/components/layout/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../../../public/logo1.png";
+import "./Footer.css";
+
+const Footer = () => (
+  <div className="footer-container">
+    <img src={logo} alt="Logo" className="logo" />
+    <p className="copyright">Copyright &copy; 2024 | Spacio</p>
+  </div>
+);
+
+export default Footer;
