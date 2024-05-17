f00ea1d0ba7e9c02e179f1f1d9af187111b5e7c2
JPRam89 <juan.ramireza@usach.cl>
2024-05-17 13:11:29 -0400

    correccion search

--- src/components/pages/home/search/Search.jsx
+++ src/components/pages/home/search/Search.jsx
@@ -1,41 +1,26 @@
 import React from "react";
-import { FilterList } from "@mui/icons-material"; // Importar el icono de filtrar de MUI
+import { FilterList } from "@mui/icons-material";
 import "./Search.css";
 
 const Search = () => {
-  return (
-    <div className="search-container">
-      <h2>
-        Encuentra, explora y descubre espacios de manera rápida y sencilla
-      </h2>
-      <div class="filter">
-        <div>
-          <FilterList className="filter-icon" />
+    return (
+        <div className="search-container">
+            <h2>Encuentra, explora y descubre espacios de manera rápida y sencilla</h2>
+            <div>
+                <div className="filter-container">
+                    <FilterList className="filter-icon" />
+                    <span className="filter-text">Filtrar</span>
+                </div>
+                <div className="input-container">
+                    <input type="text" placeholder="Category1..." className="search-input" />
+                    <input type="text" placeholder="Category2..." className="search-input" />
+                    <input type="text" placeholder="Category3..." className="search-input" />
+                    <button className="search-button">Buscar</button>
+                </div>
+            </div>
         </div>
-        <div>
-          <span>Filtrar</span>
-        </div>
-        <input
-          type="text"
-          placeholder="Category1..."
-          className="search-input"
-        />
-        <input
-          id="element2"
-          type="text"
-          placeholder="Category2..."
-          className="search-input 2"
-        />
-        <input
-          id="element3"
-          type="text"
-          placeholder="Category3..."
-          className="search-input 3"
-        />
-        <button className="search-button">Buscar</button>
-      </div>
-    </div>
-  );
+    );
 };
+
 
 export default Search;
